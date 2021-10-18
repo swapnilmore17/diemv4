5c2de4f472ea4e69ad0adb2b39ce3d2b3da612bf
Akshay Nair <84533184+okkhoyyy@users.noreply.github.com>
2021-10-17 22:52:52 -0400

    Leader election

--- LeaderElection.py
+++ LeaderElection.py
@@ -1,5 +1,6 @@
 from ledger import Ledger
 from pacemaker import Pacemaker
+import math, random
 class LeaderElection:
     def __init__(self,validators,window_size,exclude_size,reputation_leaders):
         self.validators = validators     #The list of current validators
@@ -8,34 +9,30 @@ class LeaderElection:
         self.reputation_leaders = reputation_leaders    #Map from round numbers to leaders elected due to the reputation scheme
 
     def elect_reputation_leader(self,qc):
-        active_validators = None # validators that signed the last window size committed blocks 
-        last_authors = None # ordered set of authors of last exclude size committed blocks 
+        active_validators = [] # validators that signed the last window size committed blocks 
+        last_authors = [] # ordered set of authors of last exclude size committed blocks 
         current_qc = qc
-        ####
-        for i = 0; i < self.window_size or last_authors < self.exclude_size; i = i + 1):
+        i = 0
+        while i<self.window_size and len(last_authors) < self.exclude_size:
             current_block = Ledger.committed_block(current_qc.vote_info.parent_id)
             block_author = current_block.author
             if i < self.window_size:
-                #####
-                active_validators = active_validators ∪ current_qc.signatures.signers() 
-                # |current qc.signatures.signers()| ≥ 2f + 1
-            if |last authors| < exclude_size :
-                #####
-                last_authors = last_authors ∪ {block_author}
-            current_qc = current_block.qc    ####
-        active_validators = active_validators \ last_authors 
-        #contains at least 1 validator return active validators.pick one(seed ← qc.voteinfo.round)
-    
+                active_validators.append(current_qc.signatures.signers()) 
+            if len(last_authors) < self.exclude_size :
+                last_authors.append(block_author)
+            current_qc = current_block.qc
+        for x in last_authors:
+            active_validators.remove(x)
+        return active_validators[int(random.seed(current_qc.vote_info.round)*len(active_validators))]
+        
     def update_leaders(self,qc):
         extended_round = qc.vote_info.parent_round
         qc_round = qc.vote_info.round
-        current_round = PaceMaker.current_round
+        current_round = Pacemaker.current_round
         if extended_round + 1 == qc_round and qc_round + 1 == current_round :
-            reputation_leaders[current_round + 1] = elect_reputation_leader(qc)
+            self.reputation_leaders[current_round + 1] = self.elect_reputation_leader(qc)
     
     def get_leader(self,round):
-         #######
-        if ⟨round, leader⟩ ∈ reputation_leaders :
-            return leader  #Reputation-based leader
-            #####
-        return validators[⌊round/2⌋ mod |validators|] # Round-robin leader (two rounds per leader)
+        if self.reputation_leaders[round]:
+            return self.reputation_leaders[round]
+        return self.validators[math.floor(round/2)] % len(self.validators)
